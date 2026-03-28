3fdd96a6790277499fdb33c776393f7661e38b14
Nikita R <2446589+mekedron@users.noreply.github.com>
2026-03-28 21:13:32 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,50 @@
 hs.loadSpoon("FifineDisplay")
 spoon.FifineDisplay:start()
+
+-- Window Management
+hs.hotkey.bind({"cmd", "shift"}, "2", function()
+    local output = hs.execute("/opt/homebrew/bin/ocr")
+
+    -- trim whitespace/newlines
+    output = output and output:gsub("^%s*(.-)%s*$", "%1")
+
+    if output ~= "" then
+        hs.alert.show(output)
+    else
+        hs.alert.show("OCR returned nothing")
+    end
+end)
+
+hs.hotkey.bind({"alt"}, "1", function()
+    hs.application.launchOrFocus("Chromium")
+end)
+
+hs.hotkey.bind({"alt"}, "B", function()
+    hs.application.launchOrFocus("Chromium")
+end)
+
+hs.hotkey.bind({"alt"}, "G", function()
+    hs.application.launchOrFocus("Ghostty")
+end)
+
+hs.hotkey.bind({"alt"}, "Z", function()
+    hs.application.launchOrFocus("Zed")
+end)
+
+-- ä / Ä
+hs.hotkey.bind({"alt"}, "a", function()
+    hs.eventtap.keyStrokes(" ä")
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "a", function()
+    hs.eventtap.keyStrokes(" Ä")
+end)
+
+-- ö / Ö
+hs.hotkey.bind({"alt"}, "o", function()
+    hs.eventtap.keyStrokes("ö")
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "o", function()
+    hs.eventtap.keyStrokes("Ö")
+end)
